7dc5cbc050dfc1fc6a9986e6023fbdb7e616144d
Rudy Godoy <rudygodoyg@gmail.com>
2013-09-11 13:35:48 -0500

    Initial report

---
+++ src/calculator/Calculator.java
@@ -0,0 +1,130 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+/**
+ *
+ * @author rudygodoy
+ */
+public class Calculator {
+
+    public static boolean precedence(char a, char b) {
+        String high = "*/", low = "+-";
+        if (a == '(') {
+            return false;
+        }
+        if (a == ')' && b == '(') {
+            System.out.println(")-(");
+            return false;
+        }
+        if (b == '(') {
+            return false;
+        }
+        if (b == ')') {
+            return true;
+        }
+        if (high.indexOf(a) > - 1 && low.indexOf(b) > - 1) {
+            return true;
+        }
+        if (high.indexOf(a) > - 1 && high.indexOf(b) > - 1) {
+            return true;
+        }
+        if (low.indexOf(a) > - 1 && low.indexOf(b) > - 1) {
+            return true;
+        }
+        return false;
+    }
+
+    public static String convert_to_postfix(String expr) {
+        Stack< Character> op_stack = new Stack< Character>();
+        StringBuilder out = new StringBuilder();
+        String opers = "+-*/()";
+        char top_sym = '+';
+        boolean empty;
+        String[] tokens = expr.split(" ");
+
+        for (int i = 0; i < tokens.length; i++) {
+            if (opers.indexOf(tokens[i].charAt(0)) == - 1) {
+                out.append(tokens[i]);
+                out.append(' ');
+            } else {
+                while (!(empty = op_stack.isEmpty()) && precedence(top_sym =
+                        op_stack.pop(), tokens[i].charAt(0))) {
+                    out.append(top_sym);
+                    out.append(' ');
+                }
+                if (!empty) {
+                    op_stack.push(top_sym);
+                }
+                if (empty || tokens[i].charAt(0) != ')') {
+                    op_stack.push(tokens[i].charAt(0));
+                } else {
+                    top_sym = op_stack.pop();
+                }
+            }
+        }
+        while (!op_stack.isEmpty()) {
+            out.append(op_stack.pop());
+            out.append(' ');
+        }
+        return out.toString();
+    }
+
+    public static double process_postfix(String postfix, HashMap< String, Integer> map) {
+        Stack< Double> stack = new Stack< Double>();
+        String opers = "+-*/";
+        String[] tokens = postfix.split(" ");
+        for (int i = 0; i < tokens.length; i++) // If token is a number or variable
+        {
+            if (opers.indexOf(tokens[i].charAt(0)) == - 1) {
+                double term = 0.;
+                try {
+                    term = Double.parseDouble(tokens[i]);
+                } catch (NumberFormatException ex) {
+                    term = map.get(tokens[i]);
+                }
+                stack.push(term);
+
+                // If token is an operator
+            } else {
+                double b = stack.pop(), a = stack.pop();
+                if (tokens[i].charAt(0) == '+') {
+                    a = a + b;
+                } else if (tokens[i].charAt(0) == '-') {
+                    a = a - b;
+                } else if (tokens[i].charAt(0) == '*') {
+                    a = a * b;
+                } else if (tokens[i].charAt(0) == '/') {
+                    a = a / b;
+                }
+                stack.push(a);
+            }
+        }
+        return stack.pop();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String op = "20 / 2 + 33 - 5";
+        String infix = "C * 9 / 5 + 32";
+        
+        String postfix = convert_to_postfix(infix);
+        String opos = convert_to_postfix(op);
+        System.out.println("Infix:   " + infix + " : " + op);
+        System.out.println("Postfix: " + postfix + " : " + opos);
+        HashMap< String, Integer> map = new HashMap< String, Integer>();
+        HashMap< String, Integer> map2 = new HashMap< String, Integer>();
+        for (int i = 0; i <= 100; i += 10) {
+            map.put("C", i);
+            System.out.println("C is " + i + ",  F is " + process_postfix(postfix, map));
+            System.out.println("C is " + i + ",  F is " + process_postfix(opos, map));
+        }
+    }
+}
